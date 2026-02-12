8ee23635d7cf5a3f4199c1463a887b7ffe616170
Theml <teddelol247@gmail.com>
2026-02-12 17:12:08 -0300

    feat: adicionar novos conteúdos e detalhes ao manual de integração RNDS

--- src/app/cursos/digisus/page.tsx
+++ src/app/cursos/digisus/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import { SideBar } from "@/components/SideBar";
-import InteractionButtons from "@/components/InteractionButtons";
 
 // Tipos
 type ConteudoItem = {
